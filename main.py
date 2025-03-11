e47363972e60c03a9b6cc14fa752ce72b4bb19f8
mirenk0 <MirkoDSov@gmail.com>
2025-03-11 11:54:02 +0100

    Read txt content

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+
+        return file_contents
+
+
+def main():
+    txt_content = get_book_text("./books/frankenstein.txt")
+    print(txt_content)
+
+
+main()
