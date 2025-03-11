8b2a809f2c2e186dd12e10e2eadf730ab90aa471
mirenk0 <MirkoDSov@gmail.com>
2025-03-11 12:32:16 +0100

    Restructure orderings and processing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import num_words, char_count
+from stats import num_words, char_count, formatted_char_count
 
 
 def get_book_text(filepath):
@@ -9,10 +9,18 @@ def get_book_text(filepath):
 
 
 def main():
+    print("=========== BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
     txt_content = get_book_text("./books/frankenstein.txt")
-    print(f"{num_words(txt_content)} words found in the document")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words(txt_content)} total words")
     word_count = char_count(txt_content)
-    print(f"{word_count}")
+    print("--------- Character Count -------")
+    char_list = formatted_char_count(word_count)
+    for i in char_list:
+        if i["char"].isalpha():
+            print(f"{i['char']}: {i['count']}")
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -13,3 +13,14 @@ def char_count(txt_content):
             count_dict[c_lowered] = 1
 
     return count_dict
+
+
+def formatted_char_count(count_dict):
+    dict_list = []
+
+    for k, c in count_dict.items():
+        dict_list.append({"char": k, "count": c})
+
+    dict_list.sort(key=lambda d: d["count"], reverse=True)
+
+    return dict_list
